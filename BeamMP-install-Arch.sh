213e7bd47378648a59c13e7c3ddb3e942af995a5
Teilenh <125691903+Teilenh@users.noreply.github.com>
2025-06-15 20:20:39 +0200

    Update BeamMP-install-Arch.sh

--- BeamMP-install-Arch.sh
+++ BeamMP-install-Arch.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
+
+set -e
+
+# ~~~ CONFIGURATION ~~~
+VCPKG_DIR="$HOME/vcpkg"
+BEAMMP_DIR="$HOME/BeamMP-Launcher"
+
+echo "=== [1/6] Mise à jour des paquets et installation des outils nécessaires ==="
+sudo pacman -Syu cmake git curl
 
